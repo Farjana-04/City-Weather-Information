efe73da9a9e08fe4fa4028d5567d2a676b96cac7
Farjana Akhter <farjana.akhter085@gmail.com>
2023-07-27 12:05:52 -0500

    added some code for weather icon and save in local storage, search history in html and js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,17 @@ let searchBtnEl = $("#search-btn");
 let searchInputEl = $("#search-input");
 let presentWeatherEl = $("#weather-information");
 let weatherForecastEl = $("#days-history");
-let searchHistory = [];
-
 
 const API_KEY = "f6db0a47aab47705d7072b6b5344e991";
+let searchHistory = [];
 
 function searchHandle() {
   let cityName = searchInputEl.val();
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&days=5`;
+  // console.log(url)
   fetch(url)
     .then(function (response) {
-      if (!response.ok) {
-        throw new Error("City not found!");
-      }
       return response.json();
     })
     .then(function (data) {
@@ -26,29 +23,46 @@ function searchHandle() {
     })
 
 }
-searchInputEl.on("keypress", function (event) {
-  if (event.key === "Enter") {
-    searchHandle();
-  }
-});
-
-
 searchBtnEl.on("click", searchHandle);
 
 function processData(data) {
   // Current weather data
+  let weatherArray = data.list;
   let cityName = data.name;
-  let currentDate = new Date(data.dt * 1000).toLocaleDateString();
-  let currentTemperature = Math.round(data.main.temp - 273.15);
-  let currentHumidity = data.main.humidity;
-  let currentIcon = data.weather[0].icon;
-  let currentWindSpeed = data.wind.speed;
+  //  let weatherDataToSave = [];
+
+  for (let i = 0; i < 5; i++) {
+    let thisCard = document.querySelector("#day" + i)
+    let weatherData = weatherArray[i * 8];
+    console.log(weatherData);
+    let date = new Date(weatherData.dt * 1000);
+    let temperature = Math.round(weatherData.main.temp - 273.15);
+    let humidity = weatherData.main.humidity;
+    let icon = weatherData.weather[0].icon;
+    
+
+    thisCard.childNodes[1].innerHTML = temperature;
+
+  // Display weather icon using OpenWeatherMap's icons
+    let iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
+    let iconEl = document.createElement("img");
+    iconEl.setAttribute("src", iconUrl);
+    iconEl.setAttribute("alt", "Weather Icon");
+
+    thisCard.children[0].appendChild(iconEl);
+ }
+ // Display current weather information
+ let currentDate = new Date().toLocaleDateString();
+ let currentTemperature = Math.round(data.main.temp - 273.15);
+ let currentHumidity = data.main.humidity;
+ let currentWindSpeed = data.wind.speed;
+ let currentIcon = data.weather[0].icon;
 
   $("#city-name").text(cityName);
-  $("#current-date").text(currentDate);
-  $("#current-temperature").text(currentTemperature);
-  $("#current-humidity").text(currentHumidity);
-  $("#current-wind-speed").text(currentWindSpeed);
+  $("#date").text(currentDate);
+  $("#temperature").text(currentTemperature);
+  $("#humidity").text(currentHumidity);
+  $("#wind-speed").text(currentWindSpeed);
 
   let currentIconUrl = `http://openweathermap.org/img/w/${currentIcon}.png`;
   let currentIconEl = document.createElement("img");
@@ -61,84 +75,25 @@ function processData(data) {
   localStorage.setItem("search", JSON.stringify(searchHistory));
   updateSearchHistory();
 
-  // 5-day weather forecast data
-  let url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&cnt=5`;
-  fetch(url)
-    .then(function (response) {
-      if (!response.ok) {
-        throw new Error("Forecast data not available.");
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      processForecastData(data);
-    })
-
-}
-function processForecastData(data) {
-  let weatherArray = data.list;
-  weatherForecastEl.empty();
-
-  for (let i = 0; i < 5; i++) {
-    let thisCard = document.querySelector("#day" + i)
-    let weatherData = weatherArray[i * 8];
-    let date = new Date(weatherData.dt * 1000).toLocaleDateString();
-    let temperature = Math.round(weatherData.main.temp - 273.15);
-    let humidity = weatherData.main.humidity;
-    let icon = weatherData.weather[0].icon;
-    let windSpeed = weatherData.wind.speed;
-
-    thisCard.childNodes[1].innerHTML = temperature
-
-
-
-    // Display weather icon using OpenWeatherMap's icons
-    let iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
-    let iconEl = document.createElement("img");
-    iconEl.setAttribute("src", iconUrl);
-    iconEl.setAttribute("alt", "Icon");
-
-    thisCard.children[0].appendChild(iconEl);
-
-
-  }
 }
 
-function renderSearchHistory() {
+// Call this function when the "Clear History" button is clicked
+ $("#clear-history-btn").on("click", clearSearchHistory);
 
-  // Call this function when the "Clear History" button is clicked
-  $("#clear-history-btn").on("click", clearSearchHistory);
-
-  // Initial update of the search history when the page loads
-  updateSearchHistory();
-}
 // On page load, retrieve weather data from local storage if available
 document.addEventListener("DOMContentLoaded", () => {
   let storedSearchHistory = localStorage.getItem("search");
   if (storedSearchHistory) {
     searchHistory = JSON.parse(storedSearchHistory);
-    updateSearchHistory();
+    
   }
 })
 
-// Save the updated search history to local storage
-localStorage.setItem("search", JSON.stringify(searchHistory));
 
-let weatherDataArray = [];
 
-function saveWeatherDataToLocalStorage() {
-  localStorage.setItem("weatherData", JSON.stringify(weatherDataArray));
-}
 
-// Save weather data to local storage whenever it's updated
-weatherDataArray.push(data); // Make sure to push the data into the array wherever you receive it.
-saveWeatherDataToLocalStorage();
-document.addEventListener("DOMContentLoaded", () => {
-  let storedData = localStorage.getItem("weatherData");
-  if (storedData) {
-    weatherDataArray = JSON.parse(storedData);
-  }
-});
+
+
 
 
 
